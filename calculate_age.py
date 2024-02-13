53ee6cb103ae7b574b8ccf4ac9c01441c8cae113
Dnofal <143069400+Dnofal@users.noreply.github.com>
2024-02-13 20:10:23 +0100

    First programs

---
+++ calculate_age.py
@@ -0,0 +1,7 @@
+
+year = input('What is your birthday year? ')
+age = 2024 - int(year) 
+print("You are " + str(age) )
+
+
+
---
+++ calculate_sum.py
@@ -0,0 +1,6 @@
+
+first_num = input('What is the first number ')
+second_num = input('What is the second number ')
+
+total = int(first_num) + int(second_num)
+print('Total is ' + str(total)  )
